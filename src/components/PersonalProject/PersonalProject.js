057c96bb48b46a95760d778e51a221f6a690c971
Eric Papell <ericpapell123@gmail.com>
2025-03-18 20:52:25 +0100

    change

--- src/components/PersonalProject/PersonalProject.js
+++ src/components/PersonalProject/PersonalProject.js
@@ -1,30 +1,65 @@
 "use client";
 import styles from "./personalProject.module.css";
 import { useState } from "react";
+import { firaCode } from "@/fonts/fonts";
 
 const data = [
   {
     id: 0,
     image: "taquina.webp",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam libero tortor, imperdiet nec ullamcorper ac, tincidunt id nisi. Fusce dignissim sem vel ultricies feugiat.",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam libero tortor, imperdiet nec ullamcorper ac, tincidunt id nisi. ",
     tecnologies: ["lorem", "ipsum"],
   },
   {
     id: 1,
     image: "petcode.webp",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam libero tortor, imperdiet nec ullamcorper ac, tincidunt id nisi. Fusce dignissim sem vel ultricies feugiat.",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam libero tortor, imperdiet nec ullamcorper ac, tincidunt id nisi. ",
     tecnologies: ["lorem", "ipsum"],
   },
   {
     id: 2,
     image: "lebleu.webp",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam libero tortor, imperdiet nec ullamcorper ac, tincidunt id nisi. Fusce dignissim sem vel ultricies feugiat.",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam libero tortor, imperdiet nec ullamcorper ac, tincidunt id nisi. ",
     tecnologies: ["lorem", "ipsum"],
   },
+  // {
+  //   id: 3,
+  //   image: "lebleu.webp",
+  //   text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam libero tortor, imperdiet nec ullamcorper ac, tincidunt id nisi. ",
+  //   tecnologies: ["lorem", "ipsum"],
+  // },
+  // {
+  //   id: 4,
+  //   image: "petcode.webp",
+  //   text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam libero tortor, imperdiet nec ullamcorper ac, tincidunt id nisi. ",
+  //   tecnologies: ["lorem", "ipsum"],
+  // },
+  // {
+  //   id: 5,
+  //   image: "lebleu.webp",
+  //   text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam libero tortor, imperdiet nec ullamcorper ac, tincidunt id nisi. ",
+  //   tecnologies: ["lorem", "ipsum"],
+  // },
+  // {
+  //   id: 6,
+  //   image: "lebleu.webp",
+  //   text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam libero tortor, imperdiet nec ullamcorper ac, tincidunt id nisi. ",
+  //   tecnologies: ["lorem", "ipsum"],
+  // },
 ];
 
 export function PersonalProject() {
   const [count, setCount] = useState(1);
+
+  function handleCountNext() {
+    setCount(count + 1);
+  }
+  function handleCountPrev() {
+    setCount(count - 1);
+  }
+  function handleDirectCount(id) {
+    setCount(id);
+  }
   return (
     <section className={styles.personalProjectSection}>
       <h2>Personal Projects</h2>
@@ -34,40 +69,56 @@ export function PersonalProject() {
           {data.map((project) => {
             if (count === project.id) {
               return (
-                <div className={styles.personalProjectIndividualActive}>
-                  <img src={data[count].image} />
-                  <p className={styles.personalProjectIndividualText}>
+                <div
+                  key={project.id}
+                  className={styles.personalProjectIndividualActive}
+                >
+                  <img src={project.image} />
+                  <p
+                    className={`${firaCode.className} ${styles.personalProjectIndividualText}`}
+                  >
                     {data[count].text}
                   </p>
                   <div className={styles.personalProjectIndividualActiveSkills}>
-                    {data[count].tecnologies.map((t) => {
-                      return <p>{t}</p>;
+                    {data[count].tecnologies.map((t, i) => {
+                      return <p key={i}>{t}</p>;
                     })}
                   </div>
                 </div>
               );
             } else {
               return (
-                <div className={styles.personalProjectIndividualInactive}>
+                <div
+                  key={project.id}
+                  onClick={() => {
+                    handleDirectCount(project.id);
+                  }}
+                  className={styles.personalProjectIndividualInactive}
+                >
                   <div className={styles.layer}></div>
-                  <img src="petCode.webp" />
+                  <img src={project.image} />
                   <div
                     className={styles.personalProjectIndividualInactiveSkills}
                   >
-                    <p>Lorem</p>
-                    <p>Lorem</p>
+                    {data[count].tecnologies.map((t, i) => {
+                      return <p key={i}>{t}</p>;
+                    })}
                   </div>
                 </div>
               );
             }
           })}
         </div>
-        <button className={styles.btnPrev}>
-          <img src="/arrow.png" />
-        </button>
-        <button className={styles.btnNext}>
-          <img src="/arrow.png" />
-        </button>
+        {count !== 0 && (
+          <button onClick={handleCountPrev} className={styles.btnPrev}>
+            <img src="/arrow.png" />
+          </button>
+        )}
+        {count !== data.length - 1 && (
+          <button onClick={handleCountNext} className={styles.btnNext}>
+            <img src="/arrow.png" />
+          </button>
+        )}
       </div>
     </section>
   );
--- src/components/PorfesionalProject/ProfesionalProject.js
+++ src/components/PorfesionalProject/ProfesionalProject.js
@@ -1,58 +1,58 @@
 import styles from "./profesionalProject.module.css";
+import { firaCode } from "@/fonts/fonts";
+
+const data = [
+  {
+    id: 0,
+    image: "petCode.webp",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam libero tortor, imperdiet nec ullamcorper ac, tincidunt id nisi.Fusce dignissim sem vel ultricies feugiat.",
+    tecnologies: ["javascript", "react"],
+  },
+  {
+    id: 1,
+    image: "lebleu.webp",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam libero tortor, imperdiet nec ullamcorper ac, tincidunt id nisi.Fusce dignissim sem vel ultricies feugiat.",
+    tecnologies: ["javascript", "react"],
+  },
+  {
+    id: 2,
+    image: "maxionline.webp",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam libero tortor, imperdiet nec ullamcorper ac, tincidunt id nisi.Fusce dignissim sem vel ultricies feugiat.",
+    tecnologies: ["javascript", "react"],
+  },
+  {
+    id: 3,
+    image: "taquina.webp",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam libero tortor, imperdiet nec ullamcorper ac, tincidunt id nisi.Fusce dignissim sem vel ultricies feugiat.",
+    tecnologies: ["javascript", "react"],
+  },
+];
 
 export function ProfesionalProject() {
   return (
     <section id="projects" className={styles.profesionalProjectSection}>
       <h2>Profesional Projects</h2>
       <div className={styles.profesionalProjectBox}>
-        <div className={styles.profesionalProjectIndividual}>
-          <img src="petCode.webp" />
-          <p className={styles.profesionalProjectIndividualText}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-            libero tortor, imperdiet nec ullamcorper ac, tincidunt id nisi.
-            Fusce dignissim sem vel ultricies feugiat.
-          </p>
-          <div className={styles.profesionalProjectIndividualSkills}>
-            <p>Lorem</p>
-            <p>Lorem</p>
-          </div>
-        </div>
-        <div className={styles.profesionalProjectIndividual}>
-          <img src="lebleu.webp" />
-          <p className={styles.profesionalProjectIndividualText}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-            libero tortor, imperdiet nec ullamcorper ac, tincidunt id nisi.
-            Fusce dignissim sem vel ultricies feugiat.
-          </p>
-          <div className={styles.profesionalProjectIndividualSkills}>
-            <p>Lorem</p>
-            <p>Lorem</p>
-          </div>
-        </div>
-        <div className={styles.profesionalProjectIndividual}>
-          <img src="maxionline.webp" />
-          <p className={styles.profesionalProjectIndividualText}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-            libero tortor, imperdiet nec ullamcorper ac, tincidunt id nisi.
-            Fusce dignissim sem vel ultricies feugiat.
-          </p>
-          <div className={styles.profesionalProjectIndividualSkills}>
-            <p>Lorem</p>
-            <p>Lorem</p>
-          </div>
-        </div>
-        <div className={styles.profesionalProjectIndividual}>
-          <img src="taquina.webp" />
-          <p className={styles.profesionalProjectIndividualText}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-            libero tortor, imperdiet nec ullamcorper ac, tincidunt id nisi.
-            Fusce dignissim sem vel ultricies feugiat.
-          </p>
-          <div className={styles.profesionalProjectIndividualSkills}>
-            <p>Lorem</p>
-            <p>Lorem</p>
-          </div>
-        </div>
+        {data.map((project) => {
+          return (
+            <div
+              key={project.id}
+              className={`${firaCode.className} ${styles.profesionalProjectIndividual}`}
+            >
+              <div className={styles.imageContainer}>
+                <img src={project.image} />
+              </div>
+              <p className={styles.profesionalProjectIndividualText}>
+                {project.text}
+              </p>
+              <div className={styles.profesionalProjectIndividualSkills}>
+                {project.tecnologies.map((t, i) => {
+                  return <p key={i}>{t}</p>;
+                })}
+              </div>
+            </div>
+          );
+        })}
       </div>
     </section>
   );
